a0524fb681c9de26fd7ee9a828512d6b4375acb2
Shawn Oakley <shawnoakley@gmail.com>
2013-12-15 01:52:34 -0500

    Initial commit.  Added mongoid config file'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '5656c719ddb634335ea981035ac0b7d8021256bd52dd8a821c2160dcfbcd47c18e433169f87a4a50f8c62f556f608d292663df916802f64c6e15d4a2bef62f12'
